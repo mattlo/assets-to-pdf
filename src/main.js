f3b089721891f00b0756c9da5c2c9b4d6008f420
Matt Lo <mattlo.developer@gmail.com>
2014-12-28 18:20:36 -0600

    fixed PDF integrity check

--- src/main.js
+++ src/main.js
@@ -66,7 +66,7 @@ http.createServer(function (req, res) {
 function pdfExists(fileName) {
 	var deferred = Q.defer(),
 		// construct path
-		filePath = path.join(__dirname, 'input', fileName + '.pdf');
+		filePath = path.join(__dirname, 'output', fileName + '.pdf');
 
 	try {
 		// check if file exists
